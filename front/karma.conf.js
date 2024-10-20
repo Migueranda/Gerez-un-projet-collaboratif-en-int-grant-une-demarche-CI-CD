963b6fed40e52b01e8c60d20df530a20e5c0f550
Migueranda Pierre <migueranda.pierre@hotmail.com>
2024-10-20 11:52:31 +0200

    modif test-backend 33

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -28,8 +28,8 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/bobapp'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' },
+        // { type: 'html' },
+        // { type: 'text-summary' },
         { type: 'lcov' }
       ]
     },
